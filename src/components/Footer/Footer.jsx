aafbfebdbb315d33e6da67a627ae52001afe49d5
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-10-14 00:01:39 +0530

    Styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import './Footer.css'
+// import { useState } from "react";
 
-export default function Footer(){
-    return(
+
+
+export default function Footer() {
+
+    return (
         <>
             <footer>
+                
                 <p>Feel free to Copy <a href="https://github.com/pritam-kr/mylinks" target="_blank" rel="noreferrer"><spna>Github</spna></a></p>
             </footer>
         </>
